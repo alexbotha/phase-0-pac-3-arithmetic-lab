0aba805aaa7019ba06f7f6260078786ad05375ea
alexbotha <alexbotha@hotmail.co.uk>
2022-05-12 12:31:28 +0100

    commit tree 5687271ada92cd7dbbfa59e46c707b796ef14e62

---
+++ index.js
@@ -0,0 +1,52 @@
+function add(a, b) {
+  return a + b
+}
+console.log(add(450, 4))
+
+function subtract(a,b) {
+return a - b
+}
+console.log(subtract(510, 6))
+
+function multiply(a,b) {
+return a * b
+}
+console.log(multiply(79130, 1))
+
+
+function divide(a,b) {
+return a / b
+}
+console.log(divide())
+
+/*
+function divide2(a,b){
+  return a * b
+}
+console.log(divide2(2.8634538152610443, 2))
+*/
+
+function increment(n){
+  return n +=1
+}
+
+function decrement(n) {
+  return n -=1
+}
+
+function makeInt(n) {
+  return n
+}
+
+function makeInt(n){
+  return parseInt(n,10)
+}
+
+function preserveDecimal(n){
+  return 2.222
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+
